86910e19955768108bfecf62cc32697d40163068
Andrei Lazar <61834317+andrei-lz@users.noreply.github.com>
2024-12-10 10:29:39 +0000

    Update BinanceBook.cpp

--- src/BinanceBook.cpp
+++ src/BinanceBook.cpp
@@ -3,7 +3,7 @@
 int main()
 {
     // Some tests / display of the above class in action.
-    // Hard-code some values for input to the class, extract them back out, assert and/or print the results.
+    // Hard-coded some values for input to the class, extracted them back out, assert and/or print the results.
 
     runTests();
 }
